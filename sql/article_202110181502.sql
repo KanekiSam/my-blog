c82ef5c80c6b53771eb74a27a814a08bcb30f8a5
0027017194 <zheng.bin3@iwhalecloud.com>
2023-01-03 16:46:29 +0800

    工程初始化

---
+++ sql/article_202110181502.sql
@@ -0,0 +1,76 @@
+INSERT INTO blog.article (title,publish_time,read_people,keyword,popularity,article_desc,image_urls,article_type_id,state,content) VALUES
+	 ('测试文','2021-09-25 23:44:48',58,'11',NULL,'11','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/31A43531-EEA2-4f3a-8A37-4E95ABAB2E33.png',2,0,NULL),
+	 ('测试','2021-08-31 20:09:02',NULL,'测试',NULL,'请',NULL,2,0,NULL),
+	 ('测试','2021-08-31 20:10:33',NULL,'测试',NULL,'请',NULL,1,0,NULL),
+	 ('测试','2021-08-31 20:10:44',NULL,'测试',NULL,'请',NULL,2,0,NULL),
+	 ('ORM 实现数据库表的增删改查','2021-09-02 10:25:33',68,'orm,数据库,mysql',NULL,'orm数据库增删改查，帮助学习数据库知识','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/0289jpHHk0o.jpg',3,0,NULL),
+	 ('typeorm使用学习记录1','2021-09-02 10:34:55',88,'typeorm',NULL,'mysql','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/0289jpHHk0o.jpg|https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/UubOYQBb8Kc.jpg',3,0,NULL),
+	 ('这是一首简单的歌','2021-09-06 19:55:28',2,'111',NULL,'111','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/7B4TPdiVTfI.jpg',5,0,NULL),
+	 ('我是认真的','2021-09-06 19:56:07',6,'11',NULL,'11','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/qKIaA41lMjM.jpg',4,0,NULL),
+	 ('123123','2021-09-06 19:58:44',12,'123',NULL,'12312','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/Kzt46MMi2Gc.jpg|https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/0289jpHHk0o.jpg',2,0,NULL),
+	 ('追海贼王','2021-09-27 10:47:56',74,'海贼王',NULL,'哦吼吼','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/image.png',6,0,NULL);
+INSERT INTO blog.article (title,publish_time,read_people,keyword,popularity,article_desc,image_urls,article_type_id,state,content) VALUES
+	 ('海贼里我最喜欢的是索隆','2021-10-15 11:24:44',12,'索隆,海贼王',NULL,'我喜欢的索隆','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/%E4%B8%8B%E8%BD%BD.jpeg-1634091898458|https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/src%3Dhttp___b-ssl.duitang.com_uploads_item_201809_10_20180910221425_jcdaz.thumb.700_0.jpg%26refer%3Dhttp___b-ssl.duitang.jpeg-1634093123232|https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/src%3Dhttp___i2.hdslb.com_bfs_archive_1de28e2c12a160256990a5fabc88a6a4d33bfe33.png%26refer%3Dhttp___i2.hdslb.jpeg-1634093977834',6,1,'<p>我对索隆的第一印象，就是一头绿油油的头发，和冷酷的表情搭配起来有一种反差萌感。</p><p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/%E4%B8%8B%E8%BD%BD.jpeg-1634091898458" width="30%" /></p><p>第一次出场就是海贼口中可怕的三把刀赏金猎人。第一次看到用嘴咬武器的人，总感觉作者就是为了耍帅的人设，确实索隆用起来很帅让人一下子就记住他了。</p><p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/src%3Dhttp___b-ssl.duitang.com_uploads_item_201702_16_20170216111816_x2urU.thumb.224_0.jpeg%26refer%3Dhttp___b-ssl.duitang.jpeg-1634092210143" /><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/src%3Dhttp___b-ssl.duitang.com_uploads_item_201809_10_20180910221425_jcdaz.thumb.700_0.jpg%26refer%3Dhttp___b-ssl.duitang.jpeg-1634093123232" width="30%" /></p><p>这样一个美男子竟然是一个天生的路痴，每次团战都是压轴出场（找不到路）哈哈。</p><p>让人有点不爽的是，索隆有个青梅竹马的古伊娜，莫名其妙的摔下楼梯死了（海贼里最敷衍的死法，路飞怎么揍不死），然后又出现了一个长像相似却又菜又爱抢镜的海兵美女（哼，对索隆他们老是揪着不放）。</p><p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/src%3Dhttp___image.mhxk.com_news_33_347733.jpg%26refer%3Dhttp___image.mhxk.jpeg-1634093195966" width="30%" /></p><p>索隆的话不多，说的最多的是成为世界第一的剑豪。第一次被鹰眼狠狠挫败，直面他的剑只为了不将后背留给敌人（胸口的伤疤就是这样来的）。鹰眼也很欣赏索隆的气度，手下留情。他第一次流出了眼泪说我再也不要输了。剑士有他的傲气，可是为了伙伴，他甘愿放下尊严请求鹰眼收他为徒，只为了变强可以保护伙伴。</p><p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/image-1634268240374.png" width="50%" /></p><p>海贼王的剧情什么都好，就是太虐主角了，每次打斗一定要打的奄奄一息，才能触发丝血反杀的技能，弹幕里总是还没开始认真呢，都在玩。我的内心呐喊，路飞你怎么还不戴帽子，索隆你怎么还不戴头巾，这样被丢来丢去的，一点不好玩。</p><p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/src%3Dhttp___i2.hdslb.com_bfs_archive_1de28e2c12a160256990a5fabc88a6a4d33bfe33.png%26refer%3Dhttp___i2.hdslb.jpeg-1634093977834" width="30%" /><br /><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/src%3Dhttp___nimg.ws.126.net__url%3Dhttp%253A%252F%252Fdingyue.ws.126.net%252F2021%252F0909%252F09d21153j00qz5rfs003zc000hs00vmc.jpg%26thumbnail%3D650x2147483647%26quality%3D80%26type%3Djpg%26refer%3Dhttp___nimg.ws.126.jpeg-1634093947132" width="30%" /></p><p>我这样没耐心的一个人竟然把海贼看到了900多集，真的是佩服我自己了（一边看一边吐槽作者，激励我看下去）。你最喜欢海贼里的谁啊？</p><p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/src%3Dhttp___pic4.zhimg.com_b7628ace20e82ec3fd706ffb3c9bf01f_b.jpg%26refer%3Dhttp___pic4.zhimg.jpeg-1634094568900" width="30%" /></p>'),
+	 ('初恋','2021-10-14 20:31:04',11,'歌曲,初恋',NULL,'初恋这首歌让人觉得悸动','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/YodH2WzN6Y-1634114332221.jp',5,1,'<p>分分钟都盼望跟他见面<br />fan fan zong dou pan mong ganta gin min<br />默默地守侯亦从来没怨<br />ma ma dei cv hao yi cong loi mu yvn<br />分分钟都渴望 与他相见<br />fan fan zong dou hao mong yv ta soeng gin<br />在路上碰著亦乐上几天</p><p><span>zoi lou soeng pong zoe yi lao soeng gei tin</span></p><p>爱恋没经验<br />oi lvn mu ging yim<br />今天初发现<br />gam tin co fa yin<br />遥遥共他见一面<br />yiu yiu gong ta gin ya min<br />那份快乐太新鲜<br />na fan fai lao tai san sin<br />我一夜失眠<br />o ya yei sa min&nbsp;<br />影子心里现<br />ying zi sam lui yin<br />问为何共他见一面<br />man wei ho gong ta gin ya min<br />美丽印象似初恋<br />mei lei yan zoeng ci co lvn<br /><br />默默望著是<br />ma ma mong zoe si<br />默默望著那目光似电<br />ma ma mong zoe na mu guong ci din<br />那刹那接触已令我倒颠<br />na sa na zi cu yi ling o dou din<br />分分钟都盼望跟他见面<br />fan fan zong dou pan mong gan ta gin min<br />默默地守侯亦从来没怨<br />ma ma dei cv hao yi cong loi mu yvn<br />分分钟都渴望 与他相见<br />fan fan zong dou hao mong yv ta soeng gin<br />在路上碰著亦乐上几天<br />zoi lou soeng pong zoe yi lao soeng gei tin<br />轻快的感觉飘上面<br />hing fai di gam gao piu soeng min<br />可爱的一个初恋<br />ho oi di ya go co lvn</p>'),
+	 ('Mac实用的干货收藏','2021-10-15 10:28:32',6,'Mac，干货',NULL,'Mac干货地址收藏','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/AB1D83AD-9BEE-4B0D-801C-26453A6552A-1634117570448.pn',4,1,'<p>1、<a href="http://mp.weixin.qq.com/mp/homepage?__biz=MzAxNzcwMTA4Ng==&amp;hid=2&amp;sn=4f42926a59f659f2c577a033d427e89c#wechat_redirect" target="_blank">相见恨晚的Mac技巧</a></p><p>2、<a href="https://iconarchive.com/tag/folder" target="_blank">文件图标设计</a></p><p>3、<a href="https://www.macwk.com/" target="_blank">Mac精品软件下载</a></p><p>4、<a href="https://support.apple.com/zh-cn/HT201236" target="_blank">Mac快捷键</a></p><p>5、<a href="https://xclient.info/?_=c057fbf9af8aff199762108427ad85f1" target="_blank">Xclient精品应用分享</a></p>'),
+	 ('测试','2021-10-14 10:03:04',NULL,'123',NULL,'123','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/qKIaA41lMj-1634176980223.jp',2,0,NULL),
+	 ('react typescript工程的eslint配置','2021-10-15 10:23:48',7,'react,typescript,eslint',NULL,'react ts的eslint配置','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/qiao-1634198585981.pn',1,1,'<p>不知道什么时候,已经不使用tslint了呢。</p><p>TSLint 是一个很棒的工具。它是一个专门为基于上述 TypeScript AST 格式而编写的 linter。</p><p>然而tslint 主要的缺点是无法兼容eslint ,现在由于 eslint 的社区已经非常完善了，而 tslint 无法使用这些社区的成果，也无法将自己的成果回馈给社区，所以 tslint 背后的支持者 Palantir 已经宣布会逐渐废弃 tslint 以支持 typescript-eslint-parser。</p><p>也就是说，typescript-eslint-parser 和 tslint 的主要区别就是，他们是基于不同 AST 的 lint 工具， typescript-eslint-parser 可以兼容 eslint 的生态，可以跟 eslint 的各种插件一起使用，而 tslint 不可以。</p><p>TSLint 背后的支持者 Palantir 在 2019 年宣布，他们将弃用 TSLint，转而支持**<code>typescript-eslint</code>**以造福社区。您可以在此处阅读更多相关信息：<a href="https://link.juejin.cn?target=https%3A%2F%2Fmedium.com%2Fpalantir%2Ftslint-in-2019-1a144c2317a9" target="_blank">https</a><a href="https://link.juejin.cn?target=https%3A%2F%2Fmedium.com%2Fpalantir%2Ftslint-in-2019-1a144c2317a9" target="_blank">://medium.com/palantir/tslint-in-2019-1a144c2317a9</a></p><p>怎么使用typescript-eslint呢?</p><p>安装</p><pre><code>&lt;xmp&gt;npm i eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin -D
+&lt;/xmp&gt;</code></pre><p>配置eslint,rules参考<a href="https://link.juejin.cn/?target=https%3A%2F%2Feslint.org%2Fdocs%2Frules%2F" target="_blank">eslint.org/docs/rules/</a><br /></p><pre><code>&lt;xmp&gt;module.exports = {  
+parser: ''@typescript-eslint/parser'', // Specifies the ESLint parser  
+extends: [    
+  ''plugin:@typescript-eslint/recommended'', // Uses the recommended rules from the @typescript-eslint/eslint-plugin    
+  ''prettier'',  
+],  
+plugins: [''eslint-plugin-react''],  
+rules: {    
+  // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs    
+  // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  // 0:"off", 1:"warn", 2:"error"    
+  semi: 2,    
+  ''@typescript-eslint/no-empty-interface'': 0,    
+  ''@typescript-eslint/ban-types'': [      
+    2,      
+    {  
+      types: {          
+        Function: {            
+          message: ''Prefer a specific function type, like `() =&gt; void`.'',          
+        },       
+       },      
+      },    
+    ],    
+   ''no-use-before-define'': 0,    
+   ''@typescript-eslint/no-use-before-define'': 2,    
+   ''no-shadow'': 0,    
+   ''@typescript-eslint/no-shadow'': 2  
+},  
+parserOptions: {    
+  sourceType: ''module'',    
+  ecmaFeatures: {      jsx: true,    },  
+},  
+settings: {    
+  react: {      
+    version: ''detect'', // Tells eslint-plugin-react to automatically detect the version of React to use    
+  },  
+},
+&lt;/xmp&gt;</code></pre><p>需要注意的是eslint有些和@typescript-eslint冲突的规则,需要调整:</p><p>解决“import React from ''react'';”会报错问题</p><pre><code>&lt;xmp&gt;''no-use-before-define'': 0,    
+''@typescript-eslint/no-use-before-define'': 2,    
+&lt;/xmp&gt;</code></pre><p>解决enum类型定义报错问题<br /></p><pre><code>&lt;xmp&gt;''no-shadow'': 0,   
+''@typescript-eslint/no-shadow'': 2
+&lt;/xmp&gt;</code></pre><p>配置vscode<br /></p><pre><code>&lt;xmp&gt;{
+  "eslint.validate": [
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact"
+  ],
+}
+&lt;/xmp&gt;</code></pre><p><img src="https://p1-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/5f30798d911244b1a8cf54aadf2705ac~tplv-k3u1fbpfcp-watermark.awebp" /><br /><br /></p>'),
+	 ('程序员当久了，会不会情商下降，颈椎病高血压肥胖地中海便秘脸色蜡黄批发粗糙长斑长皱纹白头发驼背近视眼邋遢口臭毛孔粗大。','2021-10-15 10:17:08',6,'程序员',NULL,'且行且珍惜','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/tim-1634199215085.jpe',2,1,'<p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/tim-1634212993037.jpeg" width="100%" /></p>'),
+	 ('按一天的时间来算，三分之一的时间在上班，三分之一的在睡觉，还有的三分之一减去上班路上和吃饭的时间，剩下的时间不到四分之一，这剩下的时间你在干嘛呢？','2021-10-14 20:18:23',5,'沸点,摸鱼',NULL,'摸鱼','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/159955450231-1634199701643.jp',2,1,'<p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/159955450231-1634213890655.jpg" width="30%" /></p>'),
+	 ('看到别人的短发很心动呀～老阿姨的少女心。','2021-10-15 10:19:23',10,'沸点,短发',NULL,'短发','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG11-1634199841668.jpe',2,1,'<p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG11-1634213788060.jpeg" width="30%" /><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG12-1634213803584.jpeg" width="30%" /></p>'),
+	 ('明明不说话的时候就是一个文静的美女子，一开口咋就成了女汉子，还不是这口破锣嗓子～','2021-10-15 10:29:36',4,'沸点',NULL,'开心就好','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG8-1634200004311.jpeg',2,1,'<p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG8-1634200004311.jpeg" width="30%" /></p>'),
+	 ('Mac 访达快捷键','2021-10-15 10:33:22',13,'Mac,快捷键',NULL,'快捷键查询','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/FA8995EA-4961-4A9D-AF56-5E8FE4C894C-1634201347282.png',1,1,'<p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/09266B04-BCD7-48B8-9000-DFB9CC0DE25-1634201955671.png" /></p>');
+INSERT INTO blog.article (title,publish_time,read_people,keyword,popularity,article_desc,image_urls,article_type_id,state,content) VALUES
+	 ('撸一个简单的文字跑马灯（二）','2021-10-14 19:40:10',4,'react,跑马灯',NULL,'react简单的跑马灯','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/laptop-computer_W0RUS6FWB-1634210727953.jpg',1,0,NULL),
+	 ('秋天的第一杯奶茶，懂？','2021-10-14 20:19:07',7,'沸点,奶茶',NULL,'请我喝茶吗','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG8-1634212553096.png',2,1,'<p><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG8-1634213920888.png" /><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG8-1634213924313.png" /><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG8-1634213934245.png" /><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG8-1634213937913.png" /><img src="https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/WechatIMG8-1634213942035.png" /></p>'),
+	 ('66','2021-10-15 10:09:41',NULL,'11',NULL,'11','https://bigbblog.oss-cn-beijing.aliyuncs.com/myblog/2020110510433-1634263778577.jpg',1,1,'<p>11</p>');
---
+++ sql/article_like_202110181503.sql
@@ -0,0 +1,14 @@
+INSERT INTO blog.article_like (is_like,article_id,client_ip) VALUES
+	 (1,5,'::ffff:127.0.0.1'),
+	 (1,1,'::ffff:127.0.0.1'),
+	 (1,1,'::ffff:172.21.108.136'),
+	 (1,1,'::ffff:172.21.105.136'),
+	 (1,10,'::ffff:172.21.105.136'),
+	 (1,9,'::ffff:172.21.105.136'),
+	 (1,7,'::ffff:172.21.105.136'),
+	 (1,8,'::ffff:172.21.105.136'),
+	 (1,6,'::ffff:172.21.105.136'),
+	 (1,5,'::ffff:172.21.105.136');
+INSERT INTO blog.article_like (is_like,article_id,client_ip) VALUES
+	 (1,1,'::ffff:10.128.9.69'),
+	 (1,11,'::ffff:10.128.9.69');
